07173559a2d0b13729d5eec7bdcdaf6ebfec0a08
Shelley Weeks <sweeks@amplience.com>
2022-10-07 11:58:56 +0100

    feat: production build ignore only when not a deploy hook triggered build

--- scripts/vercel-ignore-build-step.js
+++ scripts/vercel-ignore-build-step.js
@@ -1,6 +1,57 @@
-if (process.env.VERCEL_ENV === 'production' && process.env.VERCEL_GIT_COMMIT_SHA) {
-  console.log('🛑 - Build cancelled')
-  return process.exit(0);
-} 
+const https = require("https");
 
-process.exit(1);
+if (process.env.VERCEL_ENV !== "production") {
+  console.log("✅ - Build can proceed");
+  process.exit(1);
+}
+
+const req = https.request(
+  {
+    hostname: "api.vercel.com",
+    port: 443,
+    path: `/v6/now/deployments?limit=5&teamId=${process.env.VERCEL_IGNORE_BUILD_STEP_TEAM_ID}`,
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${process.env.VERCEL_IGNORE_BUILD_STEP_ACCESS_TOKEM}`,
+    },
+  },
+  (res) => {
+    let data = "";
+
+    res.on("data", (d) => {
+      data += d.toString();
+    });
+    res.on("end", (d) => {
+      let parsedData = JSON.parse(data);
+      let prodRunningFromDeployHook = false;
+
+      try {
+        prodRunningFromDeployHook = parsedData.deployments.find(
+          ({ state, meta, target }) =>
+            state === "BUILDING" &&
+            target === "production" &&
+            meta.deployHookId ===
+              process.env.VERCEL_IGNORE_BUILD_STEP_DEPLOY_HOOK_ID
+        );
+      } catch (e) {
+        console.log("e: ", e);
+        process.exit(0);
+      }
+
+      if (!prodRunningFromDeployHook) {
+        console.log("🛑 - Build cancelled");
+        process.exit(0);
+      }
+
+      console.log("✅ - Build can proceed");
+      process.exit(1);
+    });
+  }
+);
+
+req.on("error", (error) => {
+  console.error(error);
+  process.exit(0);
+});
+
+req.end();
